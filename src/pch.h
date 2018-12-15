51c078e446a2aad4ed1038cdf1280bdbdadce327
Piotr Moszkowicz <piotr@moszkowicz.pl>
2018-12-15 17:59:29 +0100

    Add precomible header

---
+++ src/pch.h
@@ -0,0 +1,12 @@
+//
+// Created by grzegorz on 15.12.18.
+//
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <memory>
+#include <algorithm>
+#include <functional>
+//BALD_ENGINE_PCH_H
