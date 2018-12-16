28f2f3549448306bd9166f8bf5c0a96863c7a464
Piotr Moszkowicz <piotr@moszkowicz.pl>
2018-12-16 23:57:34 +0100

    Add tests for split_string

---
+++ tests/test_split_string.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Piotr Moszkowicz on 2018-12-16.
+//
+
+#include "pch.h"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "utils/split_string.h"
+
+using testing::ElementsAre;
+
+TEST(SplitString, SplitByDot) { //NOLINT
+    std::vector<std::string> result = Bald::Utils::split_string("test.test", '.');
+
+    ASSERT_THAT(result, ElementsAre("test", "test"));
+}
+
+TEST(SplitString, DoubleSplitByDot) { //NOLINT
+    std::vector<std::string> result = Bald::Utils::split_string("asdf.test.test", '.');
+
+    ASSERT_THAT(result, ElementsAre("asdf", "test", "test"));
+}
+
+TEST(SplitString, NoNeededSplit) { //NOLINT
+    std::vector<std::string> result = Bald::Utils::split_string("asdftest", '.');
+
+    ASSERT_THAT(result, ElementsAre("asdftest"));
+}
