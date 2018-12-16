dac6339b8206483c803d5ea1406fbb5d9d252d38
Piotr Moszkowicz <piotr@moszkowicz.pl>
2018-12-17 00:14:45 +0100

    Fix

--- src/utils/split_string.cpp
+++ src/utils/split_string.cpp
@@ -10,7 +10,7 @@ namespace Bald::Utils {
     std::vector<std::string> split_string(const char* str_to_split, char delimiter) {
         std::vector<std::string> result;
         std::string temp;
-        auto str = static_cast<std::string>(str_to_split);
+        std::string str(str_to_split);
         std::istringstream stream(str);
 
         while (std::getline(stream, temp, delimiter)) {
