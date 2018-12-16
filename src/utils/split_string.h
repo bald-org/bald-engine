287504120ae578acb042643f406f35c6d767d4cc
Piotr Moszkowicz <piotr@moszkowicz.pl>
2018-12-17 00:51:17 +0100

    [BALDE-11] Add split string function (#12)
    * Remove old tests
    
    * Add split string implementation
    
    * Add tests for split_string
    
    * Change of naming and added documentation
    
    * Fix
    
    * Fix

---
+++ src/utils/split_string.h
@@ -0,0 +1,18 @@
+//
+// Created by Piotr Moszkowicz on 2018-12-16.
+//
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace Bald::Utils {
+    /**
+     * Functions which splits string into pieces by splitter
+     * @param str_to_split      String, which needs to be splitted
+     * @param delimiter         Char, which delimits split
+     * @return                  Vector, with splitted strings
+     */
+    [[nodiscard]] std::vector<std::string> split_string(const char* str_to_split, char delimiter);
+}
