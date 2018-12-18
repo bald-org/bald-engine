334a46b482426a6742c4a651b3789f25b38e7054
BIGbadEL <greg090912@gmail.com>
2018-12-18 14:02:08 +0100

    setting up glad

--- tests/glfw3_test.cpp
+++ tests/glfw3_test.cpp
@@ -5,9 +5,19 @@
 #include "pch.h"
 #include "gtest/gtest.h"
 
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
 TEST(GLFW, GLFW_Initialization){
+
     EXPECT_EQ(glfwInit(), GLFW_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    EXPECT_FALSE(window == NULL);
+
+    EXPECT_EQ(gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress) ), true);
+
     glfwTerminate();
+
 }
