5999c831e72ef1ce63953b0f3c56c69dba49ea70
BIGbadEL <greg090912@gmail.com>
2018-12-23 14:19:04 +0100

    adding mouse position

--- src/input/input_manager.h
+++ src/input/input_manager.h
@@ -10,20 +10,24 @@
 #define MAX_MOUSE_BUTTONS 32
 
 // TODO: Documentation and Logging errors
-// TODO: consider mouse storing mouse position in this class instead of in Window
+
 
 namespace Bald::Input {
 
     class InputManager {
         friend void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
         friend void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+        friend void cursor_position_callback(GLFWwindow *window, double xpos, double ypos);
     public:
         static void Update();
         [[nodiscard]] inline static bool isKeyPressed(int keycode) noexcept;
         [[nodiscard]] inline static bool isKeyTyped(int keycode) noexcept;
         [[nodiscard]] inline static bool isMouseButtonPressed(int buttoncode) noexcept;
         [[nodiscard]] inline static bool isMouseButtonTyped(int buttoncode) noexcept;
+        inline static void getMousePos(double& xpos, double& ypos) noexcept;
     private:
+        static double m_MouseX;
+        static double m_MouseY;
         static bool m_Keys[MAX_KEYS];
         static bool m_KeysState[MAX_KEYS];
         static bool m_KeysTyped[MAX_KEYS];
@@ -50,6 +54,24 @@ namespace Bald::Input {
     bool InputManager::isMouseButtonTyped(int buttoncode) noexcept {
         if(buttoncode >= MAX_MOUSE_BUTTONS) return false;
         return m_MouseButtonsTyped[buttoncode];
+    }
+
+    void InputManager::getMousePos(double &xpos, double &ypos) noexcept {
+        xpos = InputManager::m_MouseX;
+        ypos = InputManager::m_MouseY;
+    }
+
+    inline void key_callback([[maybe_unused]] GLFWwindow *window, int key,[[maybe_unused]] int scancode, int action,[[maybe_unused]] int mods) {
+        Bald::Input::InputManager::m_Keys[key] = action != GLFW_RELEASE;
+    }
+
+    inline void mouse_button_callback([[maybe_unused]] GLFWwindow *window, int button, int action,[[maybe_unused]] int mods) {
+        Bald::Input::InputManager::m_MouseButtons[button] = action != GLFW_RELEASE;
+    }
+
+    inline void cursor_position_callback([[maybe_unused]]GLFWwindow *window, double xpos, double ypos) {
+        Bald::Input::InputManager::m_MouseX = xpos;
+        Bald::Input::InputManager::m_MouseY = ypos;
     }
 
 }
--- tests/input_test.cpp
+++ tests/input_test.cpp
@@ -7,14 +7,43 @@
 
 #include "input_manager.h"
 
+
 TEST(InputManager, initialization){
     using namespace Bald;
     using namespace Input;
-    EXPECT_EQ(InputManager::isKeyPressed(GLFW_KEY_SPACE), false);
-    EXPECT_EQ(InputManager::isKeyTyped(GLFW_KEY_1), false);
-    EXPECT_EQ(InputManager::isMouseButtonPressed(GLFW_MOUSE_BUTTON_1), false);
-    EXPECT_EQ(InputManager::isMouseButtonTyped(GLFW_MOUSE_BUTTON_2), false);
+
+    EXPECT_FALSE(InputManager::isKeyPressed(GLFW_KEY_SPACE));
+    EXPECT_FALSE(InputManager::isKeyTyped(GLFW_KEY_1));
+    EXPECT_FALSE(InputManager::isMouseButtonPressed(GLFW_MOUSE_BUTTON_1));
+    EXPECT_FALSE(InputManager::isMouseButtonTyped(GLFW_MOUSE_BUTTON_2));
+
 }
 
-// TODO: test callbacks
+
+TEST(InputManager, callback){
+    using namespace Bald;
+    using namespace Input;
+
+
+    glfwInit();
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    EXPECT_TRUE(glfwSetKeyCallback(window, key_callback) == NULL);
+    EXPECT_TRUE(glfwSetMouseButtonCallback(window, mouse_button_callback) == NULL);
+    EXPECT_TRUE(glfwSetCursorPosCallback(window, cursor_position_callback) == NULL);
+
+    while (!glfwWindowShouldClose(window)){
+        if(InputManager::isKeyPressed(GLFW_KEY_SPACE)){
+            std::cout << "SPACE\n";
+        }
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+}
 
