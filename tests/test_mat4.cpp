13074a666977b5b39a7523bbe13c2d53bc1d3c7c
Blinkuu <Ramzes19981>
2019-02-07 12:21:27 +0100

    Multiple definition error. Push for checking purposes.

---
+++ tests/test_mat4.cpp
@@ -0,0 +1,13 @@
+//
+// Created by blinku on 07.02.19.
+//
+
+#include "pch.h"
+#include "gtest/gtest.h"
+#include "math/mat4.h"
+
+TEST(Determinant, Mat4_Det) {
+    Bald::Math::Mat4 A(1.0f);
+
+    ASSERT_FLOAT_EQ(1.0f, A.Det());
+}
