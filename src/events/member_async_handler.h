bb3a6e2150ee594f75f8644d9c868b6183d747e8
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-02-12 22:11:33 +0100

    Fix handlers

--- src/events/member_async_handler.h
+++ src/events/member_async_handler.h
@@ -6,17 +6,19 @@
 #include <future>
 
 #include "event.h"
-#include "base_handler.h"
+#include "function_handler.h"
 
 namespace Bald::Events {
     template<class T, class EventType>
-    class MemberAsyncHandler : public BaseHandler<T, EventType> {
+    class MemberAsyncHandler : public FunctionHandler<T, EventType> {
     public:
         typedef void (T::*HandlerFunction)(EventType*);
-        MemberAsyncHandler(T* instance, HandlerFunction handler_function): BaseHandler<T, EventType>(instance, handler_function) {};
+        MemberAsyncHandler(T* instance, HandlerFunction handler_function): FunctionHandler<T, EventType>(instance, handler_function) {};
     private:
         void call(Event* e) noexcept override {
-            std::async(std::launch::async, (BaseHandler<T, EventType>::m_instance->*BaseHandler<T, EventType>::m_handler_function)(static_cast<EventType*>(e)));
+            // TODO: Isn't it working sync cuz of .get() ?
+            //auto handle = std::async(std::launch::async, FunctionHandler<T, EventType>::m_instance->*FunctionHandler<T, EventType>::m_handler_function, static_cast<EventType*>(e));
+            //handle.get();
         }
     };
 }
--- src/events/member_handler.h
+++ src/events/member_handler.h
@@ -4,18 +4,18 @@
 
 #pragma once
 #include "event.h"
-#include "base_handler.h"
+#include "function_handler.h"
 
 namespace Bald::Events {
     template<class T, class EventType>
-    class MemberHandler : public BaseHandler<T, EventType> {
+    class MemberHandler : public FunctionHandler<T, EventType> {
     public:
         typedef void (T::*HandlerFunction)(EventType*);
-        MemberHandler(T* instance, HandlerFunction handler_function): BaseHandler<T, EventType>(instance, handler_function) {};
+        MemberHandler(T* instance, HandlerFunction handler_function): FunctionHandler<T, EventType>(instance, handler_function) {};
 
     private:
         void call(Event* e) noexcept override {
-            (BaseHandler<T, EventType>::m_instance->*BaseHandler<T, EventType>::m_handler_function)(static_cast<EventType*>(e));
+            (FunctionHandler<T, EventType>::m_instance->*FunctionHandler<T, EventType>::m_handler_function)(static_cast<EventType*>(e));
         }
     };
 }
