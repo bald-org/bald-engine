09f168a663659efbdb0eb8b6389b92500d6ac89b
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-03-04 05:36:58 +0100

    Update example to correspond new stuff

--- examples/event_example.cpp
+++ examples/event_example.cpp
@@ -12,16 +12,18 @@ using namespace Events;
 class Resizer {
 public:
     void init() {
-        EventBus::subscribe(this, &Resizer::onWindowResizeEvent, HandlerType::SyncHandler);
+        hId = EventBus::subscribe(this, &Resizer::onWindowResizeEvent, HandlerType::SyncHandler);
     }
 
     void deinit() {
-        EventBus::unsubscribe(this, &Resizer::onWindowResizeEvent);
+        EventBus::unsubscribe<WindowResizeEvent>(hId);
     }
 
     void onWindowResizeEvent(WindowResizeEvent* wre) {
         std::cout << wre->x << " " << wre->y << std::endl;
     }
+
+    unsigned int hId;
 };
 
 
@@ -30,12 +32,16 @@ int main() {
     r->init();
 
     EventBus::emit(new WindowResizeEvent(800, 600));
+    std::cout << EventBus::isEventInQueue(Events::WindowResizeEvent::eventId) << std::endl;
+    EventBus::runEventQueue();
 
     r->deinit();
 
     EventBus::emit(new WindowResizeEvent(400, 200));
+    EventBus::runEventQueue();
 
-    EventBus::flush();
+    EventBus::cleanUp();
+    std::cout << EventBus::isEventInQueue(Events::WindowResizeEvent::eventId) << std::endl;
     delete r;
 
     return 0;
