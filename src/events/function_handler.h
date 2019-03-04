626d55587d7b3c363133bfedd6c1a84f2183c3ea
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-03-04 05:34:05 +0100

    Use BaseHandler constructor to get hId

--- src/events/function_handler.h
+++ src/events/function_handler.h
@@ -18,19 +18,10 @@ namespace Bald::Events {
          * @param instance                  Instance of class where handler function pointer is
          * @param handler_function          Handler function pointer
          */
-        constexpr FunctionHandler(T *instance, HandlerFunction handler_function) noexcept : m_Instance(instance),
+        constexpr FunctionHandler(T *instance, HandlerFunction handler_function) noexcept : BaseHandler(
+                BaseHandler::maxHandlerId), m_Instance(instance),
                                                                                             m_HandlerFunction(
                                                                                                     handler_function) {};
-        /**
-         * @fn                              operator()
-         * @brief                           Compares instance of handler with elements of FunctionHandler class
-         * @param other_instance            Instance which we want to compare handler's to
-         * @param other_handler_function    Handler Function pointer which we want to compare handler's to
-         * @return [bool]                   Result of comparison
-         */
-        constexpr bool operator()(T *other_instance, HandlerFunction other_handler_function) const noexcept {
-            return (other_instance == m_Instance) && (other_handler_function == m_HandlerFunction);
-        }
 
     protected:
         T *m_Instance; /** Instance of class where HandlerFunction is **/
