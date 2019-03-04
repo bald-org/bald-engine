46b6377fa47f900bab1e1f1741173061fb85aa82
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-03-04 05:36:21 +0100

    Remove typeid cuz it sux, make more clears, add queue as event-buffer, several other changes

--- src/events/event_bus.h
+++ src/events/event_bus.h
@@ -5,7 +5,7 @@
 #pragma once
 
 #include <list>
-#include <typeindex>
+#include <queue>
 
 #include "pch.h"
 #include "base_handler.h"
@@ -65,16 +65,36 @@ namespace Bald::Events {
         }
 
         /**
-         * @fn                                                  clear
+         * @fn                                                  clearByEventType
          * @brief                                               Function, which clears whole vector of handlers of certain event type
          * @tparam EventType                                    Type of event
          */
         template<class EventType>
-        static void clear() {
+        static void clearByEventType() {
             static_assert(std::is_base_of<Event, EventType>::value, "Event is not the base of EventType");
-            HandlersVector *handlers = m_Subscribers[typeid(EventType)];
+            HandlersVector *handlers = m_Subscribers[EventType::eventId];
 
             EventBus::clearHandlers(handlers);
+        }
+
+        /**
+         * @fn                                                  clearEventQueue
+         * @brief                                               Function, which clears whole queue of events
+         */
+        static void clearEventQueue() {
+            std::for_each(m_EventQueue.begin(), m_EventQueue.end(), [](Event* e){
+                EventBus::clearEvent(e);
+            });
+            m_EventQueue.clear();
+        }
+
+        /**
+         * @fn                                                  cleanUp
+         * @brief                                               Function, which cleans all event related stuff
+         */
+        static void cleanUp() {
+            EventBus::clearEventQueue();
+            EventBus::flush();
         }
 
         /**
@@ -86,17 +106,7 @@ namespace Bald::Events {
         template<typename EventType>
         static void emit(EventType *e) {
             static_assert(std::is_base_of<Event, EventType>::value, "Event is not the base of EventType");
-            HandlersVector *handlers = m_Subscribers[typeid(EventType)];
-
-            if (handlers == nullptr) return;
-
-            for (auto &handler : *handlers) {
-                if (handler != nullptr) {
-                    handler->exec(e);
-                }
-            }
-
-            EventBus::clearEvent(e);
+            m_EventQueue.push_back(e);
         }
 
         /**
@@ -105,9 +115,44 @@ namespace Bald::Events {
          */
         static void flush() noexcept {
             std::for_each(m_Subscribers.begin(), m_Subscribers.end(),
-                          [](const std::pair<std::type_index, HandlersVector *> p) {
+                          [](const std::pair<unsigned int, HandlersVector *> p) {
                               EventBus::clearHandlers(p.second);
                           });
+        }
+
+        /**
+         * @fn                                                  isEventInQueue
+         * @brief                                               Function, which checks whether event is in queue
+         * @tparam EventType                                    Type of event
+         * @result Boolean                                      Presence of event
+         */
+        static bool isEventInQueue(unsigned int& eId) {
+            for(auto e : m_EventQueue) {
+                if (e->getId() == eId) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /**
+         * @fn                                                  runEventQueue
+         * @brief                                               Gets thru event queue and calls all handlers
+         */
+        static void runEventQueue() {
+            for(auto e : m_EventQueue) {
+                HandlersVector *handlers = m_Subscribers[e->getId()];
+                if (handlers == nullptr) {
+                    return;
+                }
+                for (auto &handler : *handlers) {
+                    if (handler != nullptr) {
+                        handler->exec(e);
+                    }
+                }
+                EventBus::clearEvent(e);
+            }
+            m_EventQueue.clear();
         }
 
         /**
@@ -118,54 +163,62 @@ namespace Bald::Events {
          * @param [T*] instance                                 Instance, where handler function is
          * @param [void (T::*)(EventType*)] handler_function    Pointer to function, which will be invoked at event
          * @param [HandlerType] hType                           Type of handler, which will be invoking the function
+         * @result [unsigned int]                               Handler ID
          */
         template<class T, class EventType>
-        static void subscribe(T *instance, void (T::*handler_function)(EventType *),
+        static unsigned int subscribe(T *instance, void (T::*handler_function)(EventType *),
                               HandlerType hType = HandlerType::SyncHandler) noexcept {
             static_assert(std::is_base_of<Event, EventType>::value, "Event is not the base of EventType");
-            HandlersVector *handlers = m_Subscribers[typeid(EventType)];
+            HandlersVector *handlers = m_Subscribers[EventType::eventId];
 
             if (handlers == nullptr) {
                 handlers = new HandlersVector();
-                m_Subscribers[typeid(EventType)] = handlers;
+                m_Subscribers[EventType::eventId] = handlers;
             }
 
             switch (hType) {
-                case HandlerType::SyncHandler:
-                    handlers->push_back(new MemberHandler<T, EventType>(instance, handler_function));
-                    break;
-                case HandlerType::AsyncHandler:
-                    handlers->push_back(new MemberAsyncHandler<T, EventType>(instance, handler_function));
-                    break;
+                case HandlerType::SyncHandler: {
+                    auto mh = new MemberHandler<T, EventType>(instance, handler_function);
+                    handlers->push_back(mh);
+                    return mh->handlerId;
+                }
+                case HandlerType::AsyncHandler: {
+                    auto mha = new MemberAsyncHandler<T, EventType>(instance, handler_function);
+                    handlers->push_back(mha);
+                    return mha->handlerId;
+                }
+                default:
+                    return 0; /** It's needed to shut the warning up **/
             }
         }
 
         /**
          * @fn                                                  unsubscribe
          * @brief                                               Function, via which you can remove handler from event
-         * @tparam T                                            Type of instance, where handler function is
          * @tparam EventType                                    Type of event to remove handler from
-         * @param [T*] instance                                 Instance, where handler function is
-         * @param [void (T::*)(EventType*)] handler_function    Pointer to function, which will be invoked at event
+         * @param [unsigned int] hId                            ID of Handler
          */
-        template<class T, class EventType>
-        static void unsubscribe(T *instance, void (T::*handler_function)(EventType *)) {
+        template<class EventType>
+        static void unsubscribe(unsigned int hId) {
             static_assert(std::is_base_of<Event, EventType>::value, "Event is not the base of EventType");
-            HandlersVector *handlers = m_Subscribers[typeid(EventType)];
+            HandlersVector *handlers = m_Subscribers[EventType::eventId];
 
-            for (auto h = handlers->begin(), e = handlers->end(); h != e;) {
-                auto h_casted = dynamic_cast<FunctionHandler<T, EventType> *>(*h);
-                if ((*h_casted)(instance, handler_function)) {
-                    delete h_casted;
-                    h = handlers->erase(h);
-                } else
-                    ++h;
+            unsigned int i = 0;
+
+            for (auto h : *handlers) {
+                if ((*h)(hId)) {
+                    handlers->erase(handlers->begin() + i);
+                    EventBus::clearHandler(h);
+                }
+                ++i;
             }
         }
 
     private:
-        static std::unordered_map<std::type_index, HandlersVector *> m_Subscribers; /** Map of event types to it's handlers list **/
+        static std::unordered_map<unsigned int, HandlersVector *> m_Subscribers; /** Map of event types to it's handlers list **/
+        static std::deque<Event*> m_EventQueue; /** Queue of events to handle **/
     };
 
-    std::unordered_map<std::type_index, HandlersVector *> EventBus::m_Subscribers;
+    std::unordered_map<unsigned int, HandlersVector *> EventBus::m_Subscribers;
+    std::deque<Event*> EventBus::m_EventQueue;
 }
