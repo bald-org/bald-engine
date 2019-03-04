ffe3b75892d68b31ae936b8fa7b867bdd05c9f51
Piotr Moszkowicz <piotr@moszkowicz.pl>
2019-03-04 05:33:27 +0100

    Change to class, implement getId function

--- src/events/window_resize_event.h
+++ src/events/window_resize_event.h
@@ -8,10 +8,11 @@
 
 namespace Bald::Events {
     /**
-     * @struct                              WindowResizeEvent
+     * @class                              WindowResizeEvent
      * @brief                               Event, which will get invoked when user changes window's size
      */
-    struct WindowResizeEvent : public Event {
+    class WindowResizeEvent : public Event {
+    public:
         /**
          * @fn                              WindowResizeEvent
          * @brief                           Constructor of WindowResizeEvent
@@ -20,7 +21,18 @@ namespace Bald::Events {
          */
         inline constexpr WindowResizeEvent(unsigned int x, unsigned int y) noexcept : x(x), y(y) {}
 
+        /**
+         * @fn                              getId
+         * @return [unsigned int]           Id of Event
+         */
+        unsigned int getId() override {
+            return eventId;
+        }
+
+        static unsigned int eventId;
         unsigned int x; /** X coordinate of new window's size **/
         unsigned int y; /** Y coordinate of new window's size **/
     };
+
+    unsigned int WindowResizeEvent::eventId = 1;
 }
