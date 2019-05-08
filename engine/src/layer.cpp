9d74e9ecc777b8984c5f642de2d08d4963af2497
Łukasz Gut <blinekgames@gmail.com>
2019-05-08 16:41:50 +0200

    Implemented simple Layer class.

---
+++ engine/src/layer.cpp
@@ -0,0 +1,26 @@
+//
+// Created by blinku on 08.05.19.
+//
+
+#include "layer.h"
+
+namespace Bald {
+
+    void Layer::Attach() noexcept {
+        m_Attached = true;
+    }
+
+    void Layer::Detach() noexcept {
+        m_Attached = false;
+    }
+
+    void Layer::RunEvents() {
+        //m_EventManager.Flush();
+    }
+
+    bool Layer::IsAttached() const noexcept {
+        return m_Attached;
+    }
+
+}
+
---
+++ engine/src/layer.h
@@ -0,0 +1,30 @@
+//
+// Created by blinku on 08.05.19.
+//
+
+#pragma once
+
+namespace Bald {
+
+    class Layer {
+    public:
+        Layer() = default;
+
+        virtual ~Layer() = default;
+
+        void Attach() noexcept;
+
+        void Detach() noexcept;
+
+        virtual void RunEvents();
+
+        virtual void Update() = 0;
+
+        [[nodiscard]] bool IsAttached() const noexcept;
+
+    private:
+        bool m_Attached = true;
+        //EventManager m_EventManager;
+    }; // END OF CLASS Layer
+
+} // END OF NAMESPACE Bald
