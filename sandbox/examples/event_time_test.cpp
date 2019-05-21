03b0179b75c44856a0667e00a10e8da81c786920
Łukasz Gut <blinekgames@gmail.com>
2019-05-21 12:44:26 +0200

    Fixed conflicts

--- sandbox/examples/event_time_test.cpp
+++ sandbox/examples/event_time_test.cpp
@@ -25,14 +25,14 @@ int main() {
     int x = 0;
     em.Subscribe<Bald::KeyEvent>(Bald::HandleType::SYNC, sub, std::reference_wrapper(x));
     high_resolution_clock::time_point t1 = high_resolution_clock::now();
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < E; j++) {
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < E; j++) {
             Bald::EventManager::Emit<Bald::KeyEvent>(static_cast<unsigned >(GLFW_KEY_0));
         }
         em.Flush();
         Bald::EventManager::ClearEventQueue();
     }
     high_resolution_clock::time_point t2 = high_resolution_clock::now();
-    std::cout << std::to_string(N) << " events -> " << std::to_string(E) << " times took: " << duration_cast<duration<double>>(t2 - t1).count() << " s\n\n";
+    std::cout << std::to_string(N) << " events -> " << std::to_string(E) << " times took: "
+              << duration_cast<duration<double>>(t2 - t1).count() << " s\n\n";
 }
-
