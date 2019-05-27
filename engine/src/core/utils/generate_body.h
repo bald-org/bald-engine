f6a0797cbe9979e4ef007ba891390b9905ff3de2
Łukasz Gut <blinekgames@gmail.com>
2019-05-27 21:45:57 +0200

    Added macro documentation.

--- engine/src/core/utils/generate_body.h
+++ engine/src/core/utils/generate_body.h
@@ -4,15 +4,55 @@
 
 #pragma once
 
+/*
+ * @def DERIVED
+ * @brief A macro definition. It is used as a flag to mark that the class inherits from another one.
+ */
 #define DERIVED IS_DERIVED
+
+/*
+ * @def DERIVED
+ * @brief A macro definition. It is used as a flag to mark that the class is a base for another ones.
+ */
+
 #define BASE IS_BASE
 
-#define DEF_OVERRIDE(x) DEF_OVERRIDE_##x
-#define DEF_OVERRIDE_IS_DERIVED public: \
+/*
+ * @def DEF_CLASS_TYPE
+ * @brief A macro definition. It defines other macro depending on the argument
+ * @param x -> Flag which will be concatenated to macro
+ */
+
+#define DEF_CLASS_TYPE(x) DEF_CLASS_TYPE_##x
+
+/*
+ * @def DEF_CLASS_TYPE_IS_DERIVED
+ * @brief A macro definition. It defines function/functions which derived class needs.
+ */
+
+#define DEF_CLASS_TYPE_IS_DERIVED public: \
                                 [[nodiscard]] inline unsigned GetType() const override { return Bald::Utils::get_type_id(*this); }
-#define DEF_OVERRIDE_IS_BASE public: \
+
+/*
+* @def DEF_CLASS_TYPE_IS_BASE
+* @brief A macro definition. It defines function/functions which base class needs.
+*/
+
+#define DEF_CLASS_TYPE_IS_BASE public: \
                              [[nodiscard]] virtual unsigned GetType() const = 0;
-#define DEF_OVERRIDE_ public: \
+
+/*
+* @def DEF_CLASS_TYPE_
+* @brief A macro definition. It defines function/functions which ordinary class needs.
+*/
+
+#define DEF_CLASS_TYPE_ public: \
                       [[nodiscard]] inline unsigned GetType() const { return Bald::Utils::get_type_id(*this); }
 
-#define GENERATE_BODY(x) DEF_OVERRIDE(x)
+/*
+* @def GENERATE_BODY
+* @brief A macro definition. It defines function/functions a class needs.
+* @param x -> Flag which is used to determine appropriate functions for certain type of class.
+*/
+
+#define GENERATE_BODY(x) DEF_CLASS_TYPE(x)
