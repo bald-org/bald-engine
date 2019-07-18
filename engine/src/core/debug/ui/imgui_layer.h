5a4e387cdb0b8ed639c23c1ae56545a6480b7920
Łukasz Gut <blinekgames@gmail.com>
2019-07-18 13:24:26 +0200

    Added documentation.

--- engine/src/core/debug/ui/imgui_layer.h
+++ engine/src/core/debug/ui/imgui_layer.h
@@ -13,23 +13,68 @@
 
 namespace Bald::Debug {
 
+    /**
+     * @class ImGuiLayer
+     * @brief In-engine implementation of abstract class Layer. This layer currently behaves as the engine's menu.
+     */
+
     class ImGuiLayer : public Layer {
-    friend class Application;
     GENERATE_BODY(DERIVED)
     public:
+
+        /**
+         * @fn ImGuiLayer
+         * @brief Default constructor.
+         */
+
         ImGuiLayer() = default;
+
+        /**
+         * @fn ImGuiLayer
+         * @brief Default destructor.
+         */
 
         ~ImGuiLayer() override = default;
 
+        /**
+         * @fn OnAttach
+         * @brief Initializes ImGui.
+         */
+
         void OnAttach() noexcept override;
+
+        /**
+         * @fn OnDetach
+         * @brief Deinitializes ImGui.
+         */
 
         void OnDetach() noexcept override;
 
+        /**
+         * @fn OnUpdate
+         * @brief Currently it is an empty method.
+         */
+
         void OnUpdate() noexcept override;
+
+        /**
+         * @fn OnRender
+         * @brief Draws menu on the screen.
+         */
 
         void OnRender() noexcept override;
 
+        /**
+         * @fn Begin
+         * @brief Begins ImGui frame.
+         */
+
         static void Begin() noexcept;
+
+        /**
+         * @fn End
+         * @brief Ends ImGui frame.
+         */
 
         static void End() noexcept;
     }; // END OF CLASS ImGuiLayer
