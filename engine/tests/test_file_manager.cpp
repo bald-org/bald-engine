99cd4b7f6d94363e4e927911b4597602fe9588d6
Łukasz Gut <40406905+Blinkuu@users.noreply.github.com>
2019-07-24 16:12:24 +0200

    [BALDE-24] Shader abstraction (#37)
    * Fixed missing assertions and fixed formatting.
    
    * Added examples and implemented *probably temporary* fix to InputManager key_callback.
    
    * Fixed formatting.
    
    * Removed flag to make compilation with glm possible. *only angry reactions*
    
    * Removed glm as submodule again
    
    * Added glm as a bald-org/glm submodule where I had fixed building errors using our flags.
    
    * Adopted shader class implementation to engine.
    
    * Changed opengl version in shaders from 460 to 330, because this is the upper limit that Travis supports.
    
    * Fixed test error.
    
    * Making a shader abstraction.
    
    * Added shader abstraction layer.
    
    * Changed folder convention.
    
    * Added more uniform setters and fixed minor issues.
    
    * Merged master into shader sbranch.
    
    * Fixed segfault and method naming.

--- engine/tests/test_file_manager.cpp
+++ engine/tests/test_file_manager.cpp
@@ -8,32 +8,28 @@
 
 TEST(FileManager, GoodSmallFileOpening) { //NOLINT
 
-    std::string file_result = Bald::Utils::FileManager::ReadFile("../engine/test_file_manager.txt",
-                                                                 Bald::Utils::FileManager::Size::SMALL_FILE);
+    std::string file_result = Bald::Utils::FileManager::ReadFile("../engine/test_file_manager.txt", Bald::Utils::FileManager::Size::SMALL_FILE);
 
     EXPECT_EQ("plik testowy\ndo odczytu", file_result);
 }
 
 TEST(FileManager, GoodBigFileOpening) { //NOLINT
 
-    std::string file_result = Bald::Utils::FileManager::ReadFile("../engine/test_file_manager.txt",
-                                                                 Bald::Utils::FileManager::Size::BIG_FILE);
+    std::string file_result = Bald::Utils::FileManager::ReadFile("../engine/test_file_manager.txt", Bald::Utils::FileManager::Size::BIG_FILE);
 
     EXPECT_EQ("plik testowy\ndo odczytu", file_result);
 }
 
 TEST(FileManager, WrongSmallFileOpening) { //NOLINT
 
-    std::string file_result = Bald::Utils::FileManager::ReadFile("no_such_file.cpp",
-                                                                 Bald::Utils::FileManager::Size::SMALL_FILE);
+    std::string file_result = Bald::Utils::FileManager::ReadFile("no_such_file.cpp", Bald::Utils::FileManager::Size::SMALL_FILE);
 
-    EXPECT_EQ("Error!", file_result);
+    EXPECT_EQ("[FileManager] Couldn't open the file at path: no_such_file.cpp", file_result);
 }
 
 TEST(FileManager, WrongBigFileOpening) { //NOLINT
 
-    std::string file_result = Bald::Utils::FileManager::ReadFile("no_such_file.cpp",
-                                                                 Bald::Utils::FileManager::Size::BIG_FILE);
+    std::string file_result = Bald::Utils::FileManager::ReadFile("no_such_file.cpp", Bald::Utils::FileManager::Size::BIG_FILE);
 
-    EXPECT_EQ("Error!", file_result);
+    EXPECT_EQ("[FileManager] Couldn't get size of the file. Check if the file exists at path: no_such_file.cpp", file_result);
 }
