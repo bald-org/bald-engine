8456c7e4f6a03c17ab5cdeb3951d7e414ecbd806
Łukasz Gut <blinekgames@gmail.com>
2019-08-08 11:00:01 +0200

    First iteration of implementing renderer.

---
+++ engine/src/core/graphics/rendering/rendering_device/renderer_2d.cpp
@@ -0,0 +1,5 @@
+//
+// Created by blinku on 07.08.2019.
+//
+
+#include "renderer_2d.h"
---
+++ engine/src/core/graphics/rendering/rendering_device/renderer_2d.h
@@ -0,0 +1,16 @@
+//
+// Created by blinku on 07.08.2019.
+//
+
+#pragma once
+
+namespace Bald::Graphics {
+
+    class Renderer2D {
+    public:
+
+    }; // END OF CLASS Renderer2D
+
+} // END OF NAMESPACE Bald::Graphics
+
+
