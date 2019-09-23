e0a284e7002a6190b1b491065b599a3e36d7f79b
bigbadel <greg090912@gmail.com>
2019-09-23 15:22:12 +0200

    fixed debug info

--- scripts/travis/compile_linux.sh
+++ scripts/travis/compile_linux.sh
@@ -2,12 +2,12 @@
 
 mkdir build_debug && cd build_debug
 cmake -GNinja -DDEFINE_DEBUG=ON -DLINUX_BUILD=ON -DTRAVIS_BUILD=ON ../
-ninja
+cmake --build . --config Debug
 cd bin
 ./../../scripts/travis/run_examples.sh
 cd ../../
 mkdir build_release && cd build_release
 cmake -GNinja -DDEFINE_RELEASE=ON -DLINUX_BUILD=ON -DTRAVIS_BUILD=ON ../
-ninja
+cmake --build . --config Release
 cd bin
 ./../../scripts/travis/run_examples.sh 
