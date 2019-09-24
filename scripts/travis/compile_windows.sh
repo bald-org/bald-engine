50af1310259e728caeac06ff6c0a39724a9a8515
bigbadel <greg090912@gmail.com>
2019-09-24 12:35:41 +0200

    enabled multi thread build on windows

--- scripts/travis/compile_windows.sh
+++ scripts/travis/compile_windows.sh
@@ -2,8 +2,8 @@
 
 mkdir build_debug && cd build_debug
 ../cmake-3.15.3-win32-x86/bin/cmake.exe -DDEFINE_DEBUG=ON -DWINDOWS_BUILD=ON -DTRAVIS_BUILD=ON ../
-../cmake-3.15.3-win32-x86/bin/cmake.exe --build . --config Debug
+../cmake-3.15.3-win32-x86/bin/cmake.exe --build . -j12 --config Debug
 cd ../
 mkdir build_release && cd build_release
 ../cmake-3.15.3-win32-x86/bin/cmake.exe -DDEFINE_RELEASE=ON -DWINDOWS_BUILD=ON -DTRAVIS_BUILD=ON ../
-../cmake-3.15.3-win32-x86/bin/cmake.exe --build . --config Release
+../cmake-3.15.3-win32-x86/bin/cmake.exe --build . -j12 --config Release
