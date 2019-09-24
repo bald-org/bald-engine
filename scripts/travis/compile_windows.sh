9e6a17b01775778f533faafc8e279850ea356ba6
BIGbadEL <greg090912@gmail.com>
2019-09-24 16:11:14 +0200

    added unit test for windows

--- scripts/travis/compile_windows.sh
+++ scripts/travis/compile_windows.sh
@@ -3,7 +3,12 @@
 mkdir build_debug && cd build_debug
 ../cmake-3.15.3-win32-x86/bin/cmake.exe -DDEFINE_DEBUG=ON -DWINDOWS_BUILD=ON -DTRAVIS_BUILD=ON ../
 ../cmake-3.15.3-win32-x86/bin/cmake.exe --build . -j12 --config Debug
-cd ../
+cd bin/Debug/
+./runUnitTests
+cd ../../../
 mkdir build_release && cd build_release
 ../cmake-3.15.3-win32-x86/bin/cmake.exe -DDEFINE_RELEASE=ON -DWINDOWS_BUILD=ON -DTRAVIS_BUILD=ON ../
 ../cmake-3.15.3-win32-x86/bin/cmake.exe --build . -j12 --config Release
+cd bin/Release/
+./runUnitTests
+cd ../../../
