5cf3e4d4b3e391eb4cd395736ee8322e73240654
BIGbadEL <greg090912@gmail.com>
2019-09-30 09:24:06 +0200

    added TODO

--- engine/src/core/events/event_manager.h
+++ engine/src/core/events/event_manager.h
@@ -137,7 +137,7 @@ namespace Bald {
     private:
         std::unordered_map<unsigned, std::vector<EventHandlerInterface*>> m_CallbacksSync; /**< Unordered map of events' type indexes and associated functions which will be called synchronously */
         std::unordered_map<unsigned, std::vector<EventHandlerInterface*>> m_CallbacksAsync; /**< Unordered map of events' type indexes and associated functions which will be called asynchronously */
-        static std::vector<Event*> m_EventQueue; /**< Basically an event queue */
+        static std::vector<Event*> m_EventQueue; /**< Basically an event queue */ //TODO: consider std::basic_string but we need real game example in order to be sure what is faster.
         static int m_ReferenceCounter; /**< Number of existing EventManagers >*/
 
     }; // END OF CLASS EventManager
