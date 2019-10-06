7103998a4fa7315619fea3879da91d5a7966d0c4
Łukasz Gut <blinekgames@gmail.com>
2019-10-06 20:48:09 +0200

    Added osx to travis

---
+++ scripts/travis/compile_osx.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+mkdir build_debug && cd build_debug
+../cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake -GNinja -DDEFINE_DEBUG=ON -DLINUX_BUILD=ON -DTRAVIS_BUILD=ON ../
+../cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake --build . --config Debug
+cd bin
+./../../scripts/travis/run_examples.sh
+exit_code=$?
+if [[ ${exit_code} != 0 ]]
+	then
+	exit ${exit_code}
+fi
+cd ../../
+mkdir build_release && cd build_release
+../cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake -GNinja -DDEFINE_RELEASE=ON -DLINUX_BUILD=ON -DTRAVIS_BUILD=ON ../
+../cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake --build . --config Release
+cd bin
+./../../scripts/travis/run_examples.sh
+exit_code=$?
+if [[ ${exit_code} != 0 ]]
+	then
+	exit ${exit_code}
+fi
