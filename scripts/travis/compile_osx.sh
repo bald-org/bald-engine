bbff3f95a3a36ca26c6ae920f98db4dc7ebd734a
Łukasz Gut <blinekgames@gmail.com>
2019-10-06 23:24:55 +0200

    fixing linking errors.

--- scripts/travis/compile_osx.sh
+++ scripts/travis/compile_osx.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir build_debug && cd build_debug
-$HOME/build/bald-org/bald-engine/cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake -GNinja -DDEFINE_DEBUG=ON -DLINUX_BUILD=ON -DTRAVIS_BUILD=ON ../
+$HOME/build/bald-org/bald-engine/cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake -GNinja -DDEFINE_DEBUG=ON -DAPPLE_BUILD=ON -DTRAVIS_BUILD=ON ../
 $HOME/build/bald-org/bald-engine/cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake --build . --config Debug
 cd bin
 ./../../scripts/travis/run_examples.sh
@@ -12,7 +12,7 @@ if [[ ${exit_code} != 0 ]]
 fi
 cd ../../
 mkdir build_release && cd build_release
-$HOME/build/bald-org/bald-engine/cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake -GNinja -DDEFINE_RELEASE=ON -DLINUX_BUILD=ON -DTRAVIS_BUILD=ON ../
+$HOME/build/bald-org/bald-engine/cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake -GNinja -DDEFINE_RELEASE=ON -DAPPLE_BUILD=ON -DTRAVIS_BUILD=ON ../
 $HOME/build/bald-org/bald-engine/cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake --build . --config Release
 cd bin
 ./../../scripts/travis/run_examples.sh
