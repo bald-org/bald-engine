b79574152af058358361501bd774c04214f2ad53
Łukasz Gut <blinekgames@gmail.com>
2019-10-06 21:21:14 +0200

    fixed cmake path

--- scripts/travis/compile_osx.sh
+++ scripts/travis/compile_osx.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir build_debug && cd build_debug
-$HOME/cmake/CMake.app/Contents/bin/cmake -GNinja -DDEFINE_DEBUG=ON -DLINUX_BUILD=ON -DTRAVIS_BUILD=ON ../
-$HOME/cmake/CMake.app/Contents/bin/cmake --build . --config Debug
+$HOME/build/bald-org/bald-engine/cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake -GNinja -DDEFINE_DEBUG=ON -DLINUX_BUILD=ON -DTRAVIS_BUILD=ON ../
+$HOME/build/bald-org/bald-engine/cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake --build . --config Debug
 cd bin
 ./../../scripts/travis/run_examples.sh
 exit_code=$?
@@ -12,8 +12,8 @@ if [[ ${exit_code} != 0 ]]
 fi
 cd ../../
 mkdir build_release && cd build_release
-$HOME/cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake -GNinja -DDEFINE_RELEASE=ON -DLINUX_BUILD=ON -DTRAVIS_BUILD=ON ../
-$HOME/cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake --build . --config Release
+$HOME/build/bald-org/bald-engine/cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake -GNinja -DDEFINE_RELEASE=ON -DLINUX_BUILD=ON -DTRAVIS_BUILD=ON ../
+$HOME/build/bald-org/bald-engine/cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake --build . --config Release
 cd bin
 ./../../scripts/travis/run_examples.sh
 exit_code=$?
