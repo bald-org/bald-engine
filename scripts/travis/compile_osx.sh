627c1d4f7f5ab99653cafa5f7bf72fdb135a2d94
Łukasz Gut <blinekgames@gmail.com>
2019-10-07 11:28:43 +0200

    removed window testing on mac

--- scripts/travis/compile_osx.sh
+++ scripts/travis/compile_osx.sh
@@ -3,21 +3,7 @@
 mkdir build_debug && cd build_debug
 $HOME/build/bald-org/bald-engine/cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake -GNinja -DDEFINE_DEBUG=ON -DAPPLE_BUILD=ON -DTRAVIS_BUILD=ON ../
 $HOME/build/bald-org/bald-engine/cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake --build . --config Debug
-cd bin
-./../../scripts/travis/run_examples.sh
-exit_code=$?
-if [[ ${exit_code} != 0 ]]
-	then
-	exit ${exit_code}
-fi
-cd ../../
+cd ../
 mkdir build_release && cd build_release
 $HOME/build/bald-org/bald-engine/cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake -GNinja -DDEFINE_RELEASE=ON -DAPPLE_BUILD=ON -DTRAVIS_BUILD=ON ../
 $HOME/build/bald-org/bald-engine/cmake-3.15.4-Darwin-x86_64/CMake.app/Contents/bin/cmake --build . --config Release
-cd bin
-./../../scripts/travis/run_examples.sh
-exit_code=$?
-if [[ ${exit_code} != 0 ]]
-	then
-	exit ${exit_code}
-fi
