57c7f9f647f12af4fcd459d670100917f8693d13
Łukasz Gut <blinekgames@gmail.com>
2019-11-23 17:11:21 +0100

    Merged master into this branch.

--- sandbox/src/sandbox.cpp
+++ sandbox/src/sandbox.cpp
@@ -6,8 +6,7 @@
 
 using namespace Bald;
 
-
-class GameLayer : public Bald::Layer {
+class GameLayer : public Layer {
 GENERATE_BODY(DERIVED)
 
 public:
@@ -31,50 +30,70 @@ public:
             0, 2, 3  // second triangle
         };
 
-        Bald::Graphics::VertexBufferLayout layout = {
-            {0, Bald::Graphics::ShaderBuiltInType::Float, Bald::Graphics::ShaderBuiltInTypeSize::Vec3, "in_Position"},
-            {1, Bald::Graphics::ShaderBuiltInType::Float, Bald::Graphics::ShaderBuiltInTypeSize::Vec4, "in_Color"},
-            {2, Bald::Graphics::ShaderBuiltInType::Float, Bald::Graphics::ShaderBuiltInTypeSize::Vec2, "in_TexCoord"}
+        Graphics::VertexBufferLayout layout = {
+            {0, Graphics::ShaderBuiltInType::Float, Graphics::ShaderBuiltInTypeSize::Vec3, "in_Position"},
+            {1, Graphics::ShaderBuiltInType::Float, Graphics::ShaderBuiltInTypeSize::Vec4, "in_Color"},
+            {2, Graphics::ShaderBuiltInType::Float, Graphics::ShaderBuiltInTypeSize::Vec2, "in_TexCoord"}
         };
 
-        m_TriangleVBO = Bald::Graphics::VertexBuffer::Create(vertices, sizeof(vertices));
+        m_TriangleVBO = Graphics::VertexBuffer::Create(vertices, sizeof(vertices));
         m_TriangleVBO->SetLayout(layout);
 
-        m_TriangleIBO = Bald::Graphics::IndexBuffer::Create(indices, sizeof(indices));
+        m_TriangleIBO = Graphics::IndexBuffer::Create(indices, sizeof(indices));
 
-        m_TriangleVAO = Bald::Graphics::VertexArray::Create();
+        m_TriangleVAO = Graphics::VertexArray::Create();
         m_TriangleVAO->AddVertexBuffer(m_TriangleVBO);
         m_TriangleVAO->AddIndexBuffer(m_TriangleIBO);
 
-        m_Shader = Bald::Graphics::Shader::Create("../engine/res/shaders/basic.vert",
+        m_Shader = Graphics::Shader::Create("../engine/res/shaders/basic.vert",
                                                   "../engine/res/shaders/basic.frag");
 
-        m_Texture = Bald::Graphics::Texture::Create("../engine/res/textures/lena.jpg");
+        m_Texture = Graphics::Texture::Create("../engine/res/textures/lena.jpg");
         // END OF TRIANGLE
     }
 
     void OnDetach() noexcept override {}
 
     void OnUpdate() noexcept override {
+        if(Input::InputManager::IsKeyPressed(GLFW_KEY_A)) {
+            m_Position.x -= m_CameraSpeed;
+        } else if(Input::InputManager::IsKeyPressed(GLFW_KEY_D)) {
+            m_Position.x += m_CameraSpeed;
+        }
+
+        if(Input::InputManager::IsKeyPressed(GLFW_KEY_W)) {
+            m_Position.y += m_CameraSpeed;
+        } else if(Input::InputManager::IsKeyPressed(GLFW_KEY_S)) {
+            m_Position.y -= m_CameraSpeed;
+        }
+
         // TRIANGLE
+        m_Camera.SetPosition(m_Position);
+        m_Shader->SetUniformMatrix4fv("u_ProjectionView", m_Camera.GetProjectionViewMatrix());
         m_Shader->Bind();
         m_Texture->Bind();
         m_TriangleVAO->Bind();
-        glDrawElements(GL_TRIANGLES, static_cast<int32_t>(m_TriangleVAO->GetIndexBuffer()->GetCount()), GL_UNSIGNED_INT, nullptr);
+        glDrawElements(GL_TRIANGLES, static_cast<int32_t>(m_TriangleVAO->GetIndexBuffer()->GetCount()), GL_UNSIGNED_INT,
+                       nullptr);
         // END TRIANGLE
     }
 
     void OnRender() noexcept override {}
 
 private:
+private:
     std::shared_ptr<Graphics::VertexArray> m_TriangleVAO = nullptr;
     std::shared_ptr<Graphics::VertexBuffer> m_TriangleVBO = nullptr;
     std::shared_ptr<Graphics::IndexBuffer> m_TriangleIBO = nullptr;
     std::shared_ptr<Graphics::Shader> m_Shader = nullptr;
     std::shared_ptr<Graphics::Texture> m_Texture = nullptr;
+
+    Graphics::Camera2D m_Camera;
+    float m_CameraSpeed = 0.005f;
+    glm::vec2 m_Position{0.0f};
 };
 
-class Sandbox : public Bald::Application {
+class Sandbox : public Application {
 GENERATE_BODY(DERIVED)
 
 public:
@@ -87,6 +106,6 @@ public:
 
 };
 
-Bald::Application* Bald::Application::Create() noexcept {
+Application* Application::Create() noexcept {
     return new Sandbox();
 }
