9f2ce42a98c541d854e502180a55b0d5bc11f1e1
Łukasz Gut <blinekgames@gmail.com>
2020-02-16 21:01:45 +0100

    Fixed examples.

--- sandbox/examples/events_example.cpp
+++ sandbox/examples/events_example.cpp
@@ -7,57 +7,53 @@
 #include "glm/glm.hpp"
 
 class ExampleLayer : public Bald::Layer {
-GENERATE_BODY(DERIVED)
+  GENERATE_BODY(DERIVED)
 
 public:
-    ExampleLayer() = default;
+  ExampleLayer() = default;
 
-    ~ExampleLayer() override = default;
+  ~ExampleLayer() override = default;
 
-    void OnAttach() noexcept override {
-        static glm::vec2 position{0.0f, 0.0f};
+  void OnAttach() noexcept override {
+    static glm::vec2 position{0.0f, 0.0f};
 
-        m_EventManager.Subscribe<Bald::KeyPressedEvent>(Bald::HandleType::SYNC, [&](const Bald::KeyPressedEvent& e) {
+    Bald::Events::EventBus::Subscribe<Bald::Events::KeyPressedEvent>(
+        [&](const Bald::Events::KeyPressedEvent &e) {
+          if (e.GetKeyCode() == BALD_KEY_W) {
+            position.y += 0.5f;
+          } else if (e.GetKeyCode() == BALD_KEY_A) {
+            position.x -= 0.5f;
+          } else if (e.GetKeyCode() == BALD_KEY_S) {
+            position.y -= 0.5f;
+          } else if (e.GetKeyCode() == BALD_KEY_D) {
+            position.x += 0.5f;
+          } else if (e.GetKeyCode() == BALD_KEY_ESCAPE) {
+            Bald::Events::EventBus::Emit<Bald::Events::WindowClosedEvent>();
+          }
 
-            if(e.GetKeyCode() == BALD_KEY_W) {
-                position.y += 0.5f;
-            } else if(e.GetKeyCode() == BALD_KEY_A) {
-                position.x -= 0.5f;
-            } else if(e.GetKeyCode() == BALD_KEY_S) {
-                position.y -= 0.5f;
-            } else if(e.GetKeyCode() == BALD_KEY_D) {
-                position.x += 0.5f;
-            } else if(e.GetKeyCode() == BALD_KEY_ESCAPE) {
-                Bald::EventManager::Emit<Bald::WindowClosedEvent>();
-            }
-
-            GAME_LOG_TRACE(std::string("Current position: [")
-                               .append(std::to_string(position.x))
-                               .append(", ")
-                               .append(std::to_string(position.y))
-                               .append("]"));
-
+          GAME_LOG_TRACE(std::string("Current position: [")
+                             .append(std::to_string(position.x))
+                             .append(", ")
+                             .append(std::to_string(position.y))
+                             .append("]"));
         });
-    }
+  }
 
-    void OnDetach() noexcept override {}
+  void OnDetach() noexcept override {}
 
-    void OnUpdate([[maybe_unused]] float deltaTime) noexcept override {}
+  void OnUpdate([[maybe_unused]] float deltaTime) noexcept override {}
 
-    void OnRender() noexcept override {}
-
+  void OnRender() noexcept override {}
 };
 
 class EventsExample : public Bald::Application {
-GENERATE_BODY(DERIVED)
+  GENERATE_BODY(DERIVED)
 public:
-    EventsExample() {
-        PushLayer<ExampleLayer>();
-    }
+  EventsExample() { PushLayer<ExampleLayer>(); }
 
-    ~EventsExample() override = default;
+  ~EventsExample() override = default;
 };
 
-Bald::Application* Bald::Application::Create() noexcept {
-    return new EventsExample();
+Bald::Application *Bald::Application::Create() noexcept {
+  return new EventsExample();
 }
